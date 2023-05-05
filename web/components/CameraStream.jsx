58a6940792a0fe4f84dec72c2f0a21940ba78738
Aryaman Parekh <aparekh5@illinois.edu>
2023-05-05 13:33:08 -0500

    add video

--- web/components/CameraStream.jsx
+++ web/components/CameraStream.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+  overflow: scroll;
+`
 const Camera = () => {
     return (
-        <div>
+        <Container>
             <h1>Camera Stream</h1>
-            <img src="http://raspberrypi_ip_address:8000/video_feed" alt="camera_stream" />
-        </div>
+            <video
+                style={{
+                    filter: 'blur(5px);'
+                }}
+                className="background-video"
+                autoPlay
+                muted
+                loop
+                playsInline
+                src={'/437.mp4'}
+            >
+                Your browser does not support the video tag.
+            </video>
+        </Container>
     );
 };
 
